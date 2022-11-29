6c9629ee711c3fdb9cd6cd932f3c0a7787aaf052
Camiel Jalink <camiel.jalink@gmail.com>
2022-11-29 21:25:32 +0100

    Added content to created typescript files

--- generateDay.js
+++ generateDay.js
@@ -4,6 +4,16 @@ let day = "day" + prompt("Which day do you wish to generate? Day:");
 
 fs.rmSync("./src/" + day, { recursive: true, force: true });
 
+const typeScriptBoilerplate = `import { readFileSync } from "fs";
+
+function advent() {
+    const stringInput = readFileSync("input.txt", "utf-8");
+    const input = stringInput.split("\\r\\n");
+    console.log(input);
+}
+
+advent();`;
+
 if (!fs.existsSync("./src/" + day)) {
     fs.mkdirSync("./src/" + day);
     const part1 = "./src/" + day + "/part1";
@@ -16,8 +26,6 @@ if (!fs.existsSync("./src/" + day)) {
 
 function createStructure(dirName) {
     fs.mkdirSync(dirName);
-    console.log(dirName);
-
-    fs.open(dirName + "/index.ts", "wx", function () {});
-    fs.open(dirName + "/input.txt", "wx", function () {});
+    fs.writeFileSync(dirName + "/index.ts", typeScriptBoilerplate);
+    fs.writeFileSync(dirName + "/input.txt", "12345");
 }
