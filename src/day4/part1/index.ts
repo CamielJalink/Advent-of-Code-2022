f310c32f65223630114b96b577749f0668cd27c4
CamielJalink <camiel.jalink@gmail.com>
2022-12-05 09:30:11 +0100

    Progress on day4

---
+++ src/day4/part1/index.ts
@@ -0,0 +1,37 @@
+import rawInput from "./input";
+
+export default function advent() {
+    const input = rawInput.split(/\n/gm);
+    console.log(numberFullyContains(input));
+}
+
+function numberFullyContains(elfPairs: string[]) {
+    let numRangeContainsOther = 0;
+    elfPairs.forEach((elfPair: string) => {
+        numRangeContainsOther += rangeContainsOther(elfPair);
+    });
+    return numRangeContainsOther;
+}
+
+function rangeContainsOther(elfPair: string) {
+    const ranges = elfPair.split(",");
+    const elf1Lower = ranges[0].split("-")[0];
+    const elf1Upper = ranges[0].split("-")[1];
+    const elf2Lower = ranges[1].split("-")[0];
+    const elf2Upper = ranges[1].split("-")[1];
+
+    let oneAreaContainsOther = 0;
+
+    if ((elf1Lower >= elf2Lower && elf1Upper <= elf2Upper) || (elf2Lower >= elf1Lower && elf2Upper <= elf1Upper)) {
+        oneAreaContainsOther++;
+    }
+    if (elf1Lower === elf2Lower && elf1Upper === elf2Upper) {
+        console.log("found one where they are equal");
+        oneAreaContainsOther = 0;
+    }
+
+    return oneAreaContainsOther;
+}
+
+// 512 en 538 waren te hoog. 486 was het ook niet.
+// Verschil tussen 512 en 486 is de 26 'exact dubbele' pairs.
