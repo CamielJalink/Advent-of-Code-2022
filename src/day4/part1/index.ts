e60c67a9ff09362fc4a1ff8c8aa6ef1ca676cd16
CamielJalink <camiel.jalink@gmail.com>
2022-12-06 20:45:49 +0100

    day4part1 solution

--- src/day4/part1/index.ts
+++ src/day4/part1/index.ts
@@ -9,29 +9,24 @@ export default function advent() {
 function numberFullyContains(elfPairs: string[]) {
     let numRangeContainsOther = 0;
     elfPairs.forEach((elfPair: string) => {
-        numRangeContainsOther += rangeContainsOther(elfPair);
+        if (rangeContainsOther(elfPair)) {
+            numRangeContainsOther++;
+        }
     });
     return numRangeContainsOther;
 }
 
 function rangeContainsOther(elfPair: string) {
     const ranges = elfPair.split(",");
-    const elf1Lower = ranges[0].split("-")[0];
-    const elf1Upper = ranges[0].split("-")[1];
-    const elf2Lower = ranges[1].split("-")[0];
-    const elf2Upper = ranges[1].split("-")[1];
+    const elf1Lower = parseInt(ranges[0].split("-")[0]);
+    const elf1Upper = parseInt(ranges[0].split("-")[1]);
+    const elf2Lower = parseInt(ranges[1].split("-")[0]);
+    const elf2Upper = parseInt(ranges[1].split("-")[1]);
 
-    let oneAreaContainsOther = 0;
+    let pairContainsOther = false;
 
-    if ((elf1Lower >= elf2Lower && elf1Upper <= elf2Upper) || (elf2Lower >= elf1Lower && elf2Upper <= elf1Upper)) {
-        oneAreaContainsOther++;
+    if ((elf1Lower <= elf2Lower && elf1Upper >= elf2Upper) || (elf2Lower <= elf1Lower && elf2Upper >= elf1Upper)) {
+        pairContainsOther = true;
     }
-    if (elf1Lower === elf2Lower && elf1Upper === elf2Upper) {
-        oneAreaContainsOther = 0;
-    }
-
-    return oneAreaContainsOther;
+    return pairContainsOther;
 }
-
-// 512 en 538 waren te hoog. 486 was het ook niet.
-// Verschil tussen 512 en 486 is de 26 'exact dubbele' pairs.
