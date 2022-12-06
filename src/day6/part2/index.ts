4e384c186ce642bd023973530e4536a8da84693b
CamielJalink <camiel.jalink@gmail.com>
2022-12-06 20:19:22 +0100

    day6part2 solution

--- src/day6/part2/index.ts
+++ src/day6/part2/index.ts
@@ -1,7 +1,35 @@
 import { readFileSync } from "fs";
 
 export default function advent() {
-    const stringInput = readFileSync("input/day6-test.txt", "utf-8");
-    const input = stringInput.split(/\n\n/gm);
-    console.log(input);
+    const input = readFileSync("input/day6.txt", "utf-8");
+    console.log(findMarker(input));
+}
+
+function findMarker(input: string) {
+    let startOfPacketMarker = 0;
+
+    for (let i = 14; i < input.length; i++) {
+        const substring = input.substring(i - 14, i);
+        if (!hasCharDouble(substring)) {
+            startOfPacketMarker = i;
+            break;
+        }
+    }
+
+    return startOfPacketMarker;
+}
+
+function hasCharDouble(substring: string) {
+    console.log(substring);
+    let hasDouble = false;
+    let buildString = "";
+    for (let i = 0; i < substring.length; i++) {
+        if (buildString.includes(substring[i])) {
+            hasDouble = true;
+            break;
+        } else {
+            buildString += substring[i];
+        }
+    }
+    return hasDouble;
 }
