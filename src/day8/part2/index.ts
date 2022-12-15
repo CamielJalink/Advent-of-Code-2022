b119ee20be321f06b706370327766308dc7df275
CamielJalink <camiel.jalink@gmail.com>
2022-12-15 14:37:22 +0100

    Day8Part2 Solution

--- src/day8/part2/index.ts
+++ src/day8/part2/index.ts
@@ -4,71 +4,50 @@ import Tree from "./tree";
 export default function advent() {
     const stringInput = readFileSync("input/day8.txt", "utf-8");
     const input = stringInput.split(/\n/gm);
-    console.log(countVisibleTrees(input));
+    console.log(findHeighestScore(input));
 }
 
-function countVisibleTrees(input: string[]) {
-    let numVisibleTrees = 0;
+function findHeighestScore(input: string[]) {
+    let highestScenicScore = 0;
     const trees: Tree[] = generateForest(input);
+    determineNeighbors(trees);
+    determineScenicScores(trees);
     trees.forEach((tree: Tree) => {
-        const y = tree.y;
+        if (tree.scenicScore > highestScenicScore) {
+            highestScenicScore = tree.scenicScore;
+        }
+    });
+    return highestScenicScore;
+}
+
+function determineScenicScores(trees: Tree[]) {
+    trees.forEach((tree: Tree) => {
+        tree.determineScenicScore();
+    });
+}
+
+function determineNeighbors(trees: Tree[]) {
+    trees.forEach((tree: Tree) => {
         const x = tree.x;
-        trees.forEach((other: Tree) => {
-            // if the other tree is to our left
-            if (other.y === y && other.x < x) {
-                if (other.height >= tree.height) {
-                    tree.isVisibleLeft = false;
-                }
-            }
-            // if the other tree is to our right
-            else if (other.y === y && other.x > x) {
-                if (other.height >= tree.height) {
-                    tree.isVisibleRight = false;
-                }
-            }
-            // tree is to the top
-            else if (other.x === x && other.y < y) {
-                if (other.height >= tree.height) {
-                    tree.isVisibleTop = false;
-                }
-            } else if (other.x === x && other.y > y) {
-                if (other.height >= tree.height) {
-                    tree.isVisibleBottom = false;
+        const y = tree.y;
+
+        trees.forEach((otherTree: Tree) => {
+            if (tree !== otherTree) {
+                const ox = otherTree.x;
+                const oy = otherTree.y;
+                if (ox - x === 1 && oy === y) {
+                    tree.rightNeighbor = otherTree;
+                } else if (ox - x === -1 && oy === y) {
+                    tree.leftNeighbor = otherTree;
+                } else if (oy - y === 1 && ox === x) {
+                    tree.bottomNeighbor = otherTree;
+                } else if (oy - y === -1 && ox === x) {
+                    tree.topNeighbor = otherTree;
                 }
             }
         });
-        // console.log(tree.x, tree.y);
-        // console.log(tree.isVisibleTop);
-        // console.log(tree.isVisibleRight);
-        // console.log(tree.isVisibleBottom);
-        // console.log(tree.isVisibleLeft);
-
-        if (tree.isVisibleBottom || tree.isVisibleTop || tree.isVisibleLeft || tree.isVisibleRight) {
-            numVisibleTrees++;
-        }
     });
-    return numVisibleTrees;
 }
-
-// function determineNeighbors(trees: Tree[]) {
-//     trees.forEach((tree: Tree) => {
-//         const x = tree.x;
-//         const y = tree.y;
-
-//         trees.forEach((otherTree: Tree) => {
-//             if (tree !== otherTree) {
-//                 const ox = otherTree.x;
-//                 const oy = otherTree.y;
-//                 // Check if the other tree is left or right
-//                 if (Math.abs(ox - x) === 1 && oy === y) {
-//                     tree.neighbors.push(otherTree);
-//                 } else if (Math.abs(oy - y) === 1 && ox === x) {
-//                     tree.neighbors.push(otherTree);
-//                 }
-//             }
-//         });
-//     });
-// }
 
 function generateForest(input: string[]) {
     const trees: Tree[] = [];
--- src/day8/part2/tree.ts
+++ src/day8/part2/tree.ts
@@ -2,11 +2,11 @@ export default class Tree {
     x: number;
     y: number;
     height: number;
-    isVisibleLeft = true;
-    isVisibleRight = true;
-    isVisibleTop = true;
-    isVisibleBottom = true;
-    // neighbors: Tree[] = [];
+    scenicScore = 0;
+    topNeighbor?: Tree;
+    bottomNeighbor?: Tree;
+    leftNeighbor?: Tree;
+    rightNeighbor?: Tree;
 
     constructor(x: number, y: number, height: string) {
         this.x = x;
@@ -14,32 +14,56 @@ export default class Tree {
         this.height = parseInt(height);
     }
 
-    // 632 is too low.
+    determineScenicScore() {
+        const numTreesLeft = this.countTreesLeft(0, this.height);
+        const numTreesRight = this.countTreesRight(0, this.height);
+        const numTreesTop = this.countTreesTop(0, this.height);
+        const numTreesBottom = this.countTreesBottom(0, this.height);
+        this.scenicScore = numTreesLeft * numTreesRight * numTreesTop * numTreesBottom;
+    }
 
-    // isVisible() {
-    //     // This tree is visible if it's on the edge of the forest.
-    //     if (this.neighbors.length < 4) {
-    //         return true;
-    //     }
+    countTreesLeft(numTreesLeft: number, treeHeight: number): number {
+        // If we don't have a left neighbor, return the accumulator up till now
+        if (!this.leftNeighbor) {
+            return numTreesLeft;
+        }
+        // If the neighbor next to us is higher then the tree we are currently considering, add that tree and return
+        else if (this.leftNeighbor.height >= treeHeight) {
+            return numTreesLeft + 1; // I can see my neighbor but no further
+        }
+        // Else, ask the neighbor to check his neighbor
+        else {
+            return this.leftNeighbor?.countTreesLeft(numTreesLeft + 1, treeHeight);
+        }
+    }
 
-    //     const smallerNeighbors: Tree[] = [];
-    //     this.neighbors.forEach((neighbor: Tree) => {
-    //         if (neighbor.height < this.height) {
-    //             smallerNeighbors.push(neighbor);
-    //         }
-    //     });
-    //     // If we have no smaller neighbors, we are not visible
-    //     if (smallerNeighbors.length === 0) {
-    //         return false;
-    //     } else {
-    //         for (let i = 0; i < smallerNeighbors.length; i++) {
-    //             // if any of our smaller neighbors is visible, then so are we!
-    //             if (smallerNeighbors[i].isVisible()) {
-    //                 return true;
-    //             }
-    //         }
-    //         // if none of our smallerNeighbors are visible, then we are also not visible.
-    //         return false;
-    //     }
-    // }
+    countTreesRight(numTreesRight: number, treeHeight: number): number {
+        if (!this.rightNeighbor) {
+            return numTreesRight;
+        } else if (this.rightNeighbor.height >= treeHeight) {
+            return numTreesRight + 1; // I can see my neighbor but no further
+        } else {
+            return this.rightNeighbor?.countTreesRight(numTreesRight + 1, treeHeight);
+        }
+    }
+
+    countTreesTop(numTreesTop: number, treeHeight: number): number {
+        if (!this.topNeighbor) {
+            return numTreesTop;
+        } else if (this.topNeighbor.height >= treeHeight) {
+            return numTreesTop + 1; // I can see my neighbor but no further
+        } else {
+            return this.topNeighbor?.countTreesTop(numTreesTop + 1, treeHeight);
+        }
+    }
+
+    countTreesBottom(numTreesBottom: number, treeHeight: number): number {
+        if (!this.bottomNeighbor) {
+            return numTreesBottom;
+        } else if (this.bottomNeighbor.height >= treeHeight) {
+            return numTreesBottom + 1; // I can see my neighbor but no further
+        } else {
+            return this.bottomNeighbor?.countTreesBottom(numTreesBottom + 1, treeHeight);
+        }
+    }
 }
