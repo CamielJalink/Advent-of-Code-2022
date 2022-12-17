10ce1251c4f0ef87dcaf5263bc8c1dc34a7b1aa4
CamielJalink <camiel.jalink@gmail.com>
2022-12-17 12:34:24 +0100

    day9part1 solution

---
+++ src/day9/part1/index.ts
@@ -0,0 +1,96 @@
+import { readFileSync } from "fs";
+
+export default function advent() {
+    const stringInput = readFileSync("input/day9.txt", "utf-8");
+    const input = stringInput.split(/\n/gm);
+    console.log(countTailLocations(input));
+}
+
+function countTailLocations(input: string[]) {
+    const tailLocations: string[] = [];
+    const headLocation = [0, 0]; // x, y
+    const tailLocation = [0, 0];
+
+    input.forEach((instruction) => {
+        const [direction, steps] = instruction.split(" ");
+
+        for (let i = 0; i < parseInt(steps); i++) {
+            switch (direction) {
+                case "U":
+                    headLocation[1]++;
+                    break;
+                case "R":
+                    headLocation[0]++;
+                    break;
+                case "D":
+                    headLocation[1]--;
+                    break;
+                case "L":
+                    headLocation[0]--;
+                    break;
+                default:
+                    console.error("illegal direction!");
+                    break;
+            }
+            determineTailLocation(headLocation, tailLocation);
+            const newTailLocation = tailLocation[0].toString() + "," + tailLocation[1].toString();
+
+            if (!tailLocations.includes(newTailLocation)) {
+                tailLocations.push(newTailLocation);
+            }
+        }
+    });
+    console.log(tailLocations);
+    return tailLocations.length;
+}
+
+function determineTailLocation(headLocation: number[], tailLocation: number[]) {
+    const headx = headLocation[0];
+    const heady = headLocation[1];
+    const tailx = tailLocation[0];
+    const taily = tailLocation[1];
+
+    // only move the tail when the head and tail aren't touching.
+    if (Math.abs(headx - tailx) <= 1 && Math.abs(heady - taily) <= 1) {
+        return tailLocation;
+    }
+
+    // If the head is to the upperright of the tail, step diagonally
+    if (heady > taily && headx > tailx) {
+        tailLocation[0]++;
+        tailLocation[1]++;
+    }
+    // If the head is to the down right of the tail, step diagonally
+    else if (heady < taily && headx > tailx) {
+        tailLocation[0]++;
+        tailLocation[1]--;
+    }
+    // If the head is to the down left of the tail, step diagonally
+    else if (heady < taily && headx < tailx) {
+        tailLocation[0]--;
+        tailLocation[1]--;
+    }
+    // If the head is to the upperleft of the tail, step diagonally
+    else if (heady > taily && headx < tailx) {
+        tailLocation[0]--;
+        tailLocation[1]++;
+    }
+    // If the head is to the right of the tail
+    else if (heady === taily && headx > tailx) {
+        tailLocation[0]++;
+    }
+    // If the head is to the left of the tail
+    else if (heady === taily && headx < tailx) {
+        tailLocation[0]--;
+    }
+    // If the head is to the top of the tail
+    else if (headx === tailx && heady > taily) {
+        tailLocation[1]++;
+    }
+    // If the head is to the bottom of the tail
+    else if (headx === tailx && heady < taily) {
+        tailLocation[1]--;
+    }
+
+    return tailLocation;
+}
--- src/startPart1.ts
+++ src/startPart1.ts
@@ -1,2 +1,2 @@
-import advent from "./day8/part1";
+import advent from "./day9/part1";
 advent();
