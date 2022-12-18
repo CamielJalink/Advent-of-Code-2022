6a75c9ab408ccd4c1df480b6d300e2549d805749
CamielJalink <camiel.jalink@gmail.com>
2022-12-18 17:14:07 +0100

    day10part1 solution

--- src/day10/part1/index.ts
+++ src/day10/part1/index.ts
@@ -1,26 +1,41 @@
 import { readFileSync } from "fs";
 
 export default function advent() {
-    const stringInput = readFileSync("input/day10-test.txt", "utf-8");
+    const stringInput = readFileSync("input/day10.txt", "utf-8");
     const input = stringInput.split(/\n/gm);
     console.log(findSumSignalStrengths(input));
 }
 
 function findSumSignalStrengths(input: string[]) {
     let xRegister = 1;
-    let cycles = 0;
+    let cycles = 1;
+
+    const cycleXvalues: number[][] = [];
 
     input.forEach((stringInstruction: string) => {
-        const [instruction, ammount] = stringInstruction.split(" ");
+        const [instruction, amount] = stringInstruction.split(" ");
 
         if (instruction === "noop") {
-            console.log("found a noop");
             cycles++;
+            cycleXvalues.push([cycles, xRegister]);
+        } else if (instruction === "addx") {
+            cycles++;
+            cycleXvalues.push([cycles, xRegister]);
+            xRegister += parseInt(amount);
+            cycles++;
+            cycleXvalues.push([cycles, xRegister]);
         }
-        // else if(instruction === "addx"){
-
-        // }
     });
 
-    return 0;
+    let totalSignalStrength = 0;
+    cycleXvalues.forEach((cycleXvalue: number[]) => {
+        const c = cycleXvalue[0];
+        const x = cycleXvalue[1];
+        if (c === 20 || c === 60 || c === 100 || c === 140 || c === 180 || c === 220) {
+            // console.log("in cycle", c, "we had xvalue", cycleXvalue[1], "with signal strength", c * cycleXvalue[1]);
+            totalSignalStrength += c * x;
+        }
+    });
+
+    return totalSignalStrength;
 }
