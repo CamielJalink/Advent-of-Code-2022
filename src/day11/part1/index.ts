002768758b7ddfb3aad1449970b55471ffc455c8
CamielJalink <camiel.jalink@gmail.com>
2022-12-24 20:15:49 +0100

    Day11part1 solution

--- src/day11/part1/index.ts
+++ src/day11/part1/index.ts
@@ -2,14 +2,33 @@ import { readFileSync } from "fs";
 import Monkey from "./monkey";
 
 export default function advent() {
-    const stringInput = readFileSync("input/day11-test.txt", "utf-8");
+    const stringInput = readFileSync("input/day11.txt", "utf-8");
     const input = stringInput.split(/\n\n/gm);
     console.log(determineMonkeyBusiness(input));
 }
 
 function determineMonkeyBusiness(strMonkeys: string[]) {
-    const monkeys: Monkey[] = [];
+    let monkeys: Monkey[] = [];
     strMonkeys.forEach((strMonkey) => {
         monkeys.push(new Monkey(strMonkey));
     });
+
+    monkeys.forEach((monkey: Monkey) => {
+        monkey.trueMonkey = monkeys.find((monk) => monk.name === monkey.trueMonkeyName);
+        monkey.falseMonkey = monkeys.find((monk) => monk.name === monkey.falseMonkeyName);
+    });
+
+    for (let i = 0; i < 20; i++) {
+        monkeys.forEach((monkey: Monkey) => {
+            monkey.handleItems();
+        });
+    }
+    monkeys.sort((a, b) => {
+        if (a.numItemsInspected >= b.numItemsInspected) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    return monkeys[0].numItemsInspected * monkeys[1].numItemsInspected;
 }
--- src/day11/part1/monkey.ts
+++ src/day11/part1/monkey.ts
@@ -3,20 +3,67 @@ import Item from "./item";
 export default class Monkey {
     name: string;
     items: Item[] = [];
+    operationType: string;
+    operationValue: string;
+    divisibleBy: number;
+    trueMonkeyName: string;
+    falseMonkeyName: string;
+    trueMonkey?: Monkey;
+    falseMonkey?: Monkey;
+    numItemsInspected = 0;
 
     constructor(strMonkey: string) {
         const input = strMonkey.split(/\n/gm);
-        this.name = input[0].split(":")[0];
+        this.name = input[0].split(" ")[1].split(":")[0];
         const startingStrItems = input[1].split(": ")[1].split(",");
         startingStrItems.forEach((strItem) => {
             this.items.push(new Item(strItem));
         });
+        const operation = input[2].split(" = old ")[1];
+        [this.operationType, this.operationValue] = operation.split(" ");
+        this.divisibleBy = parseInt(input[3].split("by ")[1]);
+        this.trueMonkeyName = input[4].split("monkey ")[1];
+        this.falseMonkeyName = input[5].split("monkey ")[1];
+    }
+
+    handleItems() {
+        this.items.forEach((item: Item) => {
+            this.doOperation(item);
+            item.worry = Math.floor(item.worry / 3);
+            this.numItemsInspected++;
+            this.doTest(item);
+        });
+        this.items = [];
+    }
+
+    doOperation(item: Item) {
+        if (this.operationValue === "old") {
+            if (this.operationType === "+") {
+                item.worry += item.worry;
+            } else if (this.operationType === "*") {
+                item.worry *= item.worry;
+            } else {
+                console.error("Illegal operationType found!");
+            }
+        } else {
+            const value = parseInt(this.operationValue);
+            if (this.operationType === "+") {
+                item.worry += value;
+            } else if (this.operationType === "*") {
+                item.worry *= value;
+            } else {
+                console.error("Illegal operationType found!");
+            }
+        }
+    }
+
+    doTest(item: Item) {
+        if (item.worry % this.divisibleBy === 0) {
+            this.trueMonkey?.items.push(item);
+            // console.log("Giving item", item.worry, "to monkey", this.trueMonkey?.name);
+        } else {
+            this.falseMonkey?.items.push(item);
+            // console.log("Giving item", item.worry, "to monkey", this.falseMonkey?.name);
+        }
     }
 }
-
-// Monkey 0:
-//   Starting items: 79, 98
-//   Operation: new = old * 19
-//   Test: divisible by 23
-//     If true: throw to monkey 2
-//     If false: throw to monkey 3
